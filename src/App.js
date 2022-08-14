8042127550a506a61aa46151133ac2558bf9088b
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-08-14 23:38:45 +0200

    style: finalization

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
 
   return (
     <div className="h-screen w-screen bg-main-dark-bg flex justify-center items-center content-center">
-      <div className='flex justify-between items-center content-center p-8'>
+      <div className='flex justify-between items-center content-center p-8 border-1 border-green-900'>
         <Optionsbreak />
         <Clock />
         <OptionsSession />
