8e90db7063d16bbf8adfdeb81aaf1280f42e5faf
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-08-14 16:08:18 +0200

    setInterval implementation

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 
@@ -8,14 +8,66 @@ const red = '#f54e4e'
 // const green = '#4aecbc'
 
 const Clock = () => {
-    const { setOptionsShow } = useStateContext()
-    const [isPaused, setIsPaused] = useState(true)
+    const { setOptionsShow, sessionLength, breakLength, setSessionLength, setBreakLength } = useStateContext()
+    const [isPaused, setIsPaused] = useState(false)
+    const [mode, setMode] = useState('session')
+    const [secondsLeft, setSecondsLeft] = useState(0)
+
+    const secondsLeftRef = useRef(secondsLeft)
+    const isPausedRef = useRef(isPaused)
+    const modeRef = useRef(mode)
 
 
+    function switchMode() {
+        const nextMode = modeRef.current === 'session' ? 'break' : 'session';
+        const nextSeconds = (nextMode === 'session' ? sessionLength : breakLength) * 60
+        setMode(nextMode)
+        modeRef.current = nextMode
+
+        setSecondsLeft(nextSeconds)
+        secondsLeftRef.current = nextSeconds
+    }
+
+    function tick() {
+        secondsLeftRef.current--
+        setSecondsLeft(secondsLeftRef.current)
+    }
+
+
+    function initTimer() {
+        setSecondsLeft(sessionLength * 60)
+    }
+
+    useEffect(() => {
+        initTimer()
+
+        const interval = setInterval(() => {
+            if (isPausedRef.current) {
+                return;
+            }
+            if (secondsLeftRef.current === 0) {
+                return switchMode()
+            }
+
+            tick()
+
+        }, 1000);
+
+        return () => clearInterval(interval)
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [sessionLength, breakLength, setBreakLength, setSessionLength])
+
+    const totalSeconds = mode === 'session' ? sessionLength * 60 : breakLength * 60
+    const percentage = Math.round(secondsLeft / totalSeconds * 100)
+
+    const minutes = Math.floor(secondsLeft / 60)
+    let seconds = secondsLeft % 60
+
+    if (seconds < 10) seconds = '0' + seconds
 
     return (
         <div className='flex flex-col items-center justify-center'>
-            <CircularProgressbar value={68} text={'60%'} styles={buildStyles({ textColor: '#fff', pathColor: red, trailColor: 'rgba(255,255,255,.2)', })} />
+            <CircularProgressbar value={percentage} text={minutes + ':' + seconds} styles={buildStyles({ textColor: '#fff', pathColor: red, trailColor: 'rgba(255,255,255,.2)', })} />
             <div>
                 {
                     isPaused ?
